9a673d8bbc60e97801b2d4b94ab702117256c0c3
Duyrep <duyreps@gmail.com>
2026-06-18 14:16:46 +0700

    feat: add start:local script for local development in package.json and turbo.json; test log access and refresh tokens in proxy.ts

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -5,6 +5,8 @@ export async function proxy(request: NextRequest) {
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
 
+  console.log(accessToken, refreshToken);
+
   const { pathname } = request.nextUrl;
   if (pathname.startsWith("/login")) {
     if (accessToken || refreshToken) {
